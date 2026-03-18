4d04743f14c8b096d0ffa8abae45aab3e0244e11
Dora Begonja <dbegonja.pmfst.hr>
2026-03-18 15:42:37 +0100

    vjezba 1

--- Vježbe/Vježbe_1/zadatak3.py
+++ Vježbe/Vježbe_1/zadatak3.py
@@ -5,7 +5,7 @@ while True:
         y1 = float(unos[1])
         break
     except:
-        print("Greška!")
+        print("Greška, probaj opet")
 while True:
     try:
         unos = input("Unesi drugu točku (x y): ").split()
@@ -13,9 +13,9 @@ while True:
         y2 = float(unos[1])
         break
     except:
-        print("Greška")
+        print("Greška, probaj opet")
 if x1 == x2:
-    print("x =", x1)  #ako su x koordinate jednake pravac je okomit na x os i ima jednadžbu x=k
+    print("x =", x1, "(pravac okomit na x os)")  #ako su x koordinate jednake pravac je okomit na x os i ima jednadžbu x=k
 else:
     k = (y2 - y1) / (x2 - x1)
     l = y1 - k * x1
---
+++ Vježbe/Vježbe_1/zadatak5.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+
+def pravac(x1, y1, x2, y2, spremi=False, ime="graf.pdf"):
+    if x1 == x2:
+        print("x =", x1, "(pravac okomit na x os)")
+        plt.axvline(x=x1) #crtanje vertikalne linije na x1 (axis vertical line)
+    else:
+        k = (y2 - y1) / (x2 - x1)
+        l = y1 - k * x1
+        print("y =", k, "x +", l)
+        x = [x1, x2]
+        y = [y1, y2]
+        plt.plot(x, y)
+    plt.scatter([x1, x2], [y1, y2])
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Pravac kroz dvije točke")
+    if spremi:
+        plt.savefig(ime)
+        print("Spremljeno kao", ime)
+    else:
+        plt.show()
+
+
+# unos podataka
+x1 = float(input("x1: "))
+y1 = float(input("y1: "))
+x2 = float(input("x2: "))
+y2 = float(input("y2: "))
+
+# izbor korisnika
+izbor = input("Upiši 's' za spremanje ili bilo što za prikaz: ")
+
+if izbor == "s":
+    ime = input("Ime datoteke (npr. graf.pdf): ")
+    pravac(x1, y1, x2, y2, True, ime)
+else:
+    pravac(x1, y1, x2, y2)
