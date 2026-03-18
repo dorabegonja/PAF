4b8ccc40f154216b0df1da9b351dc63a3398c92f
Dora Begonja <dbegonja.pmfst.hr>
2026-03-18 16:42:05 +0100

    gibanje 1

---
+++ Vježbe/Vježbe_1/gibanje1.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+F = float(input("Unesite silu u N: "))
+m = float(input("Unesite masu čestice u kg: "))
+t_max = 10  #koliko dugo simuliramo gibanje
+dt = 0.01  #vremenski korak
+n = int(t_max / dt)  #koliko koraka trebamo napravit
+t = [0]
+x = [0]  # početna pozicija
+v = [0]  # početna brzina
+a = [F/m]  # akceleracija konstantna
+#numeričko rješavanje (Eulerova metoda)
+for i in range(n):
+    t_new = t[-1] + dt  #-1 označava zadnji element liste
+    v_new = v[-1] + a[-1] * dt
+    x_new = x[-1] + v[-1] * dt
+    t.append(t_new)
+    v.append(v_new)
+    x.append(x_new)
+    a.append(a[-1])  #konstantna akceleracija
+plt.figure(figsize=(12, 8)) #veličina grafa
+#x-t graf
+plt.subplot(3,1,1) #3 reda, 1 stupac, 1. graf
+plt.plot(t, x, color='blue')
+plt.title("x − t graf")
+plt.xlabel("t [s]")
+plt.ylabel("x [m]")
+plt.grid(True)
+#v-t graf
+plt.subplot(3,1,2)
+plt.plot(t, v, color='green')
+plt.title("v − t graf")
+plt.xlabel("t [s]")
+plt.ylabel("v [m/s]")
+plt.grid(True)
+#a-t graf
+plt.subplot(3,1,3)
+plt.plot(t, a, color='red')
+plt.title("a − t graf")
+plt.xlabel("t [s]")
+plt.ylabel("a [m/s²]")
+plt.grid(True)
+plt.show()
