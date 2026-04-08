b7faca3dc9c10d05413a8d2176c0996c1f7c91b1
Dora Begonja <dbegonja.pmfst.hr>
2026-04-08 22:42:02 +0200

    zadatak 3

--- Vježbe/Vjezbe_2/calculus.py
+++ Vježbe/Vjezbe_2/calculus.py
@@ -17,26 +17,3 @@ def derivacija_interval(f, donjag, gornjag, epsilon, metoda="three-step"):
 
 def funkcija(x):
     return x**2
-
-
-
-#zadatak 4
-
-def meda(f, donjag, gornjag, N):
-    if donjag>gornjag:
-        donjag, gornjag= gornjag, donjag
-    elif donjag==gornjag:
-        return 0, 0
-    else:
-        gornja=0
-        donja=0
-        x=np.linspace(donjag, gornjag, N+1)
-        dx=(gornjag-donjag)/N
-        for i in range(N):
-            donja+=f(x)*dx 
-        print(donja)
-        for i in range(1, n+1):
-            gornja+=a
-
-def funkcija(x):
-    return x**2
