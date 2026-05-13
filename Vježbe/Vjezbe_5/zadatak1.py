150f359545fc35cb2db209e12dfa40c7203a3881
Dora Begonja <dbegonja.pmfst.hr>
2026-05-13 23:13:46 +0200

    Prvi zadatak

---
+++ Vježbe/Vjezbe_5/zadatak1.py
@@ -0,0 +1,9 @@
+rezultat_a = 5.0 - 4.935
+print("a) 5.0 - 4.935 =", rezultat_a)
+
+print("Očekivano: 0.065")
+
+
+suma = 0.1 + 0.2 + 0.3
+print("\nb) 0.1 + 0.2 + 0.3 =", suma)
+print("Je li je jednako 0.6?", suma == 0.6)
