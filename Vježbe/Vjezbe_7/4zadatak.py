8acaf8e64def83391093a87c019a358b502d561d
Dora Begonja <dbegonja.pmfst.hr>
2026-05-26 21:15:48 +0200

    cetvrti zadatak

--- Vježbe/Vjezbe_7/4zadatak.py
+++ Vježbe/Vjezbe_7/4zadatak.py
@@ -17,8 +17,8 @@ def medijan(podaci):
 sredina1 = np.mean(mase)
 medijan1 = medijan(mase)
 
-print("Sredina (sa outlierima) =", sredina1)
-print("Medijan (sa outlierima) =", medijan1)
+print("Sredina (sa pogreškama) =", sredina1)
+print("Medijan (sa pogreškama) =", medijan1)
 
 mase_bez = [x for x in mase if 1.8 < x < 2.3]
 
@@ -26,8 +26,8 @@ mase_bez = [x for x in mase if 1.8 < x < 2.3]
 sredina2 = np.mean(mase_bez)
 medijan2 = medijan(mase_bez)
 
-print("\nSredina (bez outliera) =", sredina2)
-print("Medijan (bez outliera) =", medijan2)
+print("\nSredina (bez pogrešaka) =", sredina2)
+print("Medijan (bez pogrešaka) =", medijan2)
 
 print("\nPromjena sredine =", abs(sredina2 - sredina1))
 print("Promjena medijana =", abs(medijan2 - medijan1))
@@ -35,7 +35,7 @@ print("Promjena medijana =", abs(medijan2 - medijan1))
 plt.hist(mase, bins=15, edgecolor='black')
 plt.xlabel("Masa zvijezde")
 plt.ylabel("Frekvencija")
-plt.title("Histogram svih mjerenja (sa i bez outliera)")
+plt.title("Histogram svih mjerenja (sa i bez pogrešaka)")
 
 plt.axvline(sredina1, color='red', linestyle='--', label=f"Sredina (sa) = {sredina1:.2f}")
 plt.axvline(medijan1, color='green', linestyle='--', label=f"Medijan (sa) = {medijan1:.2f}")
