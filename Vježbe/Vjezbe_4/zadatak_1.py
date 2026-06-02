3a26ba2eba58e585f9828d085d366b4677c44d4d
Dora Begonja <dbegonja.pmfst.hr>
2026-06-02 17:22:41 +0200

    elektron i pozitron

--- Vježbe/Vjezbe_4/zadatak_1.py
+++ Vježbe/Vjezbe_4/zadatak_1.py
@@ -3,13 +3,10 @@ import matplotlib.pyplot as plt
 
 m = 9.11e-31
 
-B = np.array([0, 0, 1])
-E = np.array([0, 0, 0])
-
 dt = 1e-13
-koraci = 4000
+koraci = 4000   #broj iteracija
 
-def putanja(q):
+def putanja(q, E, B):
     r = np.array([0., 0., 0.])
     v = np.array([1e5, 2e5, 3e5])
 
@@ -20,7 +17,6 @@ def putanja(q):
         y.append(r[1])
         z.append(r[2])
 
-        #Lorentzova sila
         sila = q * (E + np.cross(v, B))
         a = sila / m
 
@@ -29,18 +25,29 @@ def putanja(q):
 
     return x, y, z
 
-xe, ye, ze = putanja(-1.6e-19)
-xp, yp, zp = putanja(+1.6e-19)
+kombinacije = [
+    (np.array([0,0,0]),     np.array([0,0,1])),      #B
+    (np.array([5e5,0,0]),   np.array([0,0,1])),      #E u x smjeru
+    (np.array([0,5e5,0]),   np.array([0,0,1])),      #E u y smjeru
+    (np.array([0,0,5e5]),   np.array([0,0,1])),      #E paralelno s B
+]
 
-fig = plt.figure()
-ax = fig.add_subplot(projection="3d")
+fig = plt.figure(figsize=(12, 12))
 
-ax.plot(xe, ye, ze, label="Elektron")
-ax.plot(xp, yp, zp, label="Pozitron")
+for i, (E, B) in enumerate(kombinacije, start=1):
+    ax = fig.add_subplot(2, 2, i, projection="3d")
 
-ax.set_xlabel("x")
-ax.set_ylabel("y")
-ax.set_zlabel("z")
-ax.legend()
+    xe, ye, ze = putanja(-1.6e-19, E, B)
+    xp, yp, zp = putanja(+1.6e-19, E, B)
 
+    ax.plot(xe, ye, ze, label="Elektron")
+    ax.plot(xp, yp, zp, label="Pozitron")
+
+    ax.set_title(f"E = {E}, B = {B}")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+    ax.legend()
+
+plt.tight_layout()
 plt.show()
